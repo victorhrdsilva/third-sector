cc521951367ee7d0bb41551315f1a028c1a0485a
brunosbrito <brunobrito728@gmail.com>
2023-05-20 14:06:00 -0300

    feat: created the registration screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React, { useState } from 'react';
-import ThirdSectorProvider from './context/ThirdSectorProvider';
-import { Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import ThirdSectorProvider from "./context/ThirdSectorProvider";
+import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./pages/Register/Register";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <ThirdSectorProvider>
-      <Routes>
-          {/*<Route path="/" element={<LoginPage />}></Route>
-          <Route path='/register' element={<RegisterPage />}></Route>
-                                   <Route
-                            path="/home"
-                            element={
-                                <PrivatePage>
-                                    <Home />
-                                </PrivatePage>
-                            }
-                        /> */}
+      <>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/register" element={<Register />} />
         </Routes>
+      </>
     </ThirdSectorProvider>
   );
 }
