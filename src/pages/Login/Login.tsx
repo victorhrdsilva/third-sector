aba34e9eb345f5e9e8dc9553d8af338beb683ef5
victorhrdsilva <victorhrdsilva@gmail.com>
2023-05-24 23:47:02 -0300

    add text singin

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,32 +1,54 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { login } from "../../services/authService";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { BackgroudColor, Container, Wrapper } from "./style";
 
 const loginUserFormSchema = yup.object().shape({
-    email: yup.string().required("Email é obrigatório").email("Email inválido"),
-    password: yup.string().required("Senha é obrigatório").min(8, { message: 'Senha requer ao menos 8 dígitos' }),
+  email: yup.string().required("Email é obrigatório").email("Email inválido"),
+  password: yup
+    .string()
+    .required("Senha é obrigatório")
+    .min(8, { message: "Senha requer ao menos 8 dígitos" }),
 });
 
-type loginFormData = yup.InferType<typeof loginUserFormSchema>
+type loginFormData = yup.InferType<typeof loginUserFormSchema>;
 
 export default function Login() {
-    const { register, handleSubmit, formState: { errors } } = useForm<loginFormData>({
-        resolver: yupResolver(loginUserFormSchema),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginFormData>({
+    resolver: yupResolver(loginUserFormSchema),
+  });
 
-    async function handleLoginSubmit(data: loginFormData) {
-        login(data).then((res) => {
-            console.log(res)
-        }).catch((res) => { 
-            console.log(res)
-            alert(res.response.data);
-        })
-    }
+  async function handleLoginSubmit(data: loginFormData) {
+    login(data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((res) => {
+        console.log(res);
+        alert(res.response.data);
+      });
+  }
 
-    return (
-        <>
-        </>
-    );
+  return (
+    <Container>
+      <BackgroudColor>
+        <Wrapper>
+          <h1>Sign in to</h1>
+          <h2>Lorem Ipsum is simply</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s,
+          </p>
+        </Wrapper>
+      </BackgroudColor>
+      <BackgroudColor inputColor="true"></BackgroudColor>
+    </Container>
+  );
 }
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,1 +1,34 @@
+import styled from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  box-sizing: border-box;
+`;
+
+export const BackgroudColor = styled.div`
+  width: 100vw;
+  height: 50vh;
+  background-color: ${(props) =>
+    props.inputColor ? "var(--secundary-color)" : "var(--primary-color)"};
+`;
+
+export const Wrapper = styled.div`
+  width: 396px;
+  color: #FFFFFF;
+  margin: 150px 0 0 73px;
+  h1{
+    font-size: 34px;
+  };
+
+  h2{
+    font-size: 24px;
+  };
+
+  p{
+    font-size: 13px;
+    margin-top: 22px;
+  }
+
+`
